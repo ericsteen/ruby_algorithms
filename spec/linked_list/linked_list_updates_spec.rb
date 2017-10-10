541e47a5667abea9ace3cda3ac7e1d89b4c1f221
Eric Steen <ericsteen1@gmail.com>
2017-10-10 00:04:06 -0700

    Fix specs

--- spec/linked_list/linked_list_updates_spec.rb
+++ spec/linked_list/linked_list_updates_spec.rb
@@ -7,6 +7,21 @@ RSpec.describe LinkedList, 'Updates' do
     list
   end
 
+  it '#append inserts an item after the end of the list' do
+    expect(subject.print_list).to eq('0,1,2,3,4')
+    subject.append(5)
+    expect(subject.print_list).to eq('0,1,2,3,4,5')
+  end
+
+  it '#prepend inserts an item before the head of the list' do
+    expect(subject.print_list).to eq('0,1,2,3,4')
+    subject.prepend(-1)
+    expect(subject.print_list).to eq('-1,0,1,2,3,4')
+  end
+
+  it 'delete' do
+  end
+
   it '#reverse returns the list in opposite order' do
     expect(subject.print_list).to eq('0,1,2,3,4')
     subject.reverse
